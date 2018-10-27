73bbb736674e633cfc26239763565ead935d4c99
Mauricio Uyaguari <lmauricio12@gmail.com>
2018-10-27 15:55:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,18 +8,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>
+            React-Redux App
+          </h2>
         </header>
+        <div className="Todo-App">
+          
+        
+        
+        
+        </div> 
+
       </div>
     );
   }
